b2f7248c08c80936b1639e12517d7d5ea1e1628f
Max Margenot <max.margenot@syllable.life>
2019-05-08 17:41:40 +0000

    MAINT: added pytest to install

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.1.0',
     platforms=['mac', 'unix'],
     install_requires=['h5py', 'matplotlib', 'scipy>=0.19',
-                      'tqdm', 'numpy', 'joblib==0.11',
+                      'tqdm', 'numpy', 'joblib==0.11', 'pytest',
                       'opencv-python', 'click', 'ruamel.yaml<=0.15.0',
                       'dask[complete]', 'chest', 'seaborn', 'dask_jobqueue>=0.3.0',
                       'scikit-image>=0.14', 'bokeh'],
